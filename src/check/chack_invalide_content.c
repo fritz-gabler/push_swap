e883b1f4c86b9c467b6cd5e823c7aa1b5c200b63
Fritz Gäbler <fritzgabler@MacBook-Pro-von-Fritz.local>
2023-08-16 12:47:19 +0200

    baisc structur

---
+++ src/check/chack_invalide_content.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   chack_invalide_content.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgabler <mail@student.42heilbronn.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 19:17:25 by fgabler           #+#    #+#             */
+/*   Updated: 2023/08/15 19:52:51 by fgabler          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/push_swap.h"
+
+int check_invalide_content(int  argc, char **argv)
+{
+    if 
+}
+
+int is_content_digit(int argc, char **argv)
+{
+   int  i; 
+
+   i = 0;
+   while (i < argc)
+   {
+       
+
+   }
+
+}
